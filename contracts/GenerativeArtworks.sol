78c5793543889dc86056cb9208be2a12e9740000
Alex Supkay <asupkay1124@gmail.com>
2021-04-08 21:58:14 -0400

    Clean up

--- contracts/GenerativeArtworks.sol
+++ contracts/GenerativeArtworks.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
 
-contract GenerativeArtworks is ERC721 {
+contract GenerativeArtworks is ERC721Enumerable {
     constructor() ERC721("Generative Artworks", "GA") {}
-
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,25 @@
 require("@nomiclabs/hardhat-waffle");
+const { resolve } = require("path");
+
+require('dotenv').config({ path: resolve(__dirname, "./.env") });
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const { ALCHEMY_API_KEY, ROPSTEN_PRIVATE_KEY }  = process.env;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
